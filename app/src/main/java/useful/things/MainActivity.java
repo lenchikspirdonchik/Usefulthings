8334327f1e77e7a06bb908eb6e6bb83916cf29a5
Леонид <2003.leonid2003@gmail.com>
2019-12-21 21:26:03 +0300

    cool

--- app/src/main/java/useful/things/MainActivity.java
+++ app/src/main/java/useful/things/MainActivity.java
@@ -3,6 +3,7 @@ package useful.things;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button calculator = findViewById(R.id.btn_calc);
+        Button numeralsystems = findViewById(R.id.btn_numsys);
+        Button calcform = findViewById(R.id.btn_calcform);
+
     }
+
+
 }
