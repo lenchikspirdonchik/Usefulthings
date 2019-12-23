7edcb2d39e186aa3f4799bdf78af681848d5f18d
Леонид <2003.leonid2003@gmail.com>
2019-12-23 21:31:45 +0300

    cool

--- app/src/main/java/useful/things/MainActivity.java
+++ app/src/main/java/useful/things/MainActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button calculator;
+    Button numeralsystems;
+    Button calcform;
+    PopupMenu popupMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button calculator = (Button) findViewById(R.id.btn_calc);
-        Button numeralsystems = (Button) findViewById(R.id.btn_numsys);
-        Button calcform = (Button) findViewById(R.id.btn_calcform);
+         calculator = (Button) findViewById(R.id.btn_calc);
+         numeralsystems = (Button) findViewById(R.id.btn_numsys);
+         calcform = (Button) findViewById(R.id.btn_calcform);
+         popupMenu = new PopupMenu(this, numeralsystems);
 
 
         calculator.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +35,38 @@ public class MainActivity extends AppCompatActivity {
         numeralsystems.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NumSysCtivity.class);
-                startActivity(intent);
+
+                showPopupMenu(v);
             }
         });
+    }
+
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.inflate(R.menu.popupmenu);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu1:
+                                Intent intent = new Intent(MainActivity.this, NumSysCtivity.class);
+                                startActivity(intent);
+                                return true;
+                            case R.id.menu2:
+                                return true;
+
+                            default:
+                                return false;
+                        }
+                    }
+                });
+
+        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+            }
+        });
+        popupMenu.show();
     }
 
 
