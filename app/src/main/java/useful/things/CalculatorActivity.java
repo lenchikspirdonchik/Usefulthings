d7f11309532a4da44f9c6d72d7c169f5b24bcd9f
2003l <2003.leonid2003@gmail.com>
2019-12-26 13:16:24 +0300

    cool

--- app/src/main/java/useful/things/CalculatorActivity.java
+++ app/src/main/java/useful/things/CalculatorActivity.java
@@ -11,12 +11,11 @@ import java.util.regex.Pattern;
 public class CalculatorActivity extends AppCompatActivity {
 
     TextView txt;
-    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, b00,bpoint, bplus, bminus, bmulti, bdiv, bequel, bdelete, bdelonechar;
-    private char opt = ' ';
-    private boolean go = true, addWrite = true;
-    private double val = 0;
-    private boolean bool = false;
-    private String buffertxt = "";
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, b00, bpoint, bplus, bminus, bmulti, bdiv, bequel, bdelete, bdelonechar;
+     char opt = ' ';
+     boolean go = true, addWrite = true, bool = false;
+     double val = 1;
+     String buffertxt = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +45,7 @@ public class CalculatorActivity extends AppCompatActivity {
         bdelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              txt.setText("0");
+              txt.setText("");
                 opt = ' ';
                 val = 0;
                 buffertxt="";
@@ -68,7 +67,7 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 });
 
-        final View.OnClickListener btnnum = new View.OnClickListener() {
+        View.OnClickListener btnnum = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button numbtn = (Button) v;
@@ -85,42 +84,29 @@ public class CalculatorActivity extends AppCompatActivity {
                 go = true;
             }
         };
-        b1.setOnClickListener(btnnum);
-        b2.setOnClickListener(btnnum);
-        b3.setOnClickListener(btnnum);
-        b4.setOnClickListener(btnnum);
-        b5.setOnClickListener(btnnum);
-        b6.setOnClickListener(btnnum);
-        b7.setOnClickListener(btnnum);
-        b8.setOnClickListener(btnnum);
-        b9.setOnClickListener(btnnum);
-        b0.setOnClickListener(btnnum);
-        b00.setOnClickListener(btnnum);
 
-        final View.OnClickListener btncalculat = new View.OnClickListener() {
+        View.OnClickListener btncalculat = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button calculatbtn = (Button) v;
+                opt=calculatbtn.getText().toString().charAt(0);
                 if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", txt.getText().toString()))
                     if (go) {
-                        val = calc(val, txt.getText().toString(), calculatbtn.getText().toString().charAt(1));
+                        val = calc(val, txt.getText().toString(), opt);
                         if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
                             txt.setText(String.valueOf((int) val));
                         } else {
                             txt.setText(String.valueOf(val));
                         }
-                        opt=calculatbtn.getText().toString().charAt(1);
+                        opt=calculatbtn.getText().toString().charAt(0);
                         go = false;
                         addWrite = false;
                     } else {
-                        opt=calculatbtn.getText().toString().charAt(1);
+                        opt=calculatbtn.getText().toString().charAt(0);
                     }
             }
         };
-        bplus.setOnClickListener(btncalculat);
-        bminus.setOnClickListener(btncalculat);
-        bdiv.setOnClickListener(btncalculat);
-        bmulti.setOnClickListener(btncalculat);
+
 
 
 bpoint.setOnClickListener(new View.OnClickListener() {
@@ -151,10 +137,24 @@ bequel.setOnClickListener(new View.OnClickListener() {
             }
     }
 });
-
+        b1.setOnClickListener(btnnum);
+        b2.setOnClickListener(btnnum);
+        b3.setOnClickListener(btnnum);
+        b4.setOnClickListener(btnnum);
+        b5.setOnClickListener(btnnum);
+        b6.setOnClickListener(btnnum);
+        b7.setOnClickListener(btnnum);
+        b8.setOnClickListener(btnnum);
+        b9.setOnClickListener(btnnum);
+        b0.setOnClickListener(btnnum);
+        b00.setOnClickListener(btnnum);
+        bplus.setOnClickListener(btncalculat);
+        bminus.setOnClickListener(btncalculat);
+        bdiv.setOnClickListener(btncalculat);
+        bmulti.setOnClickListener(btncalculat);
     }
-
-    private double calc(double x, String input, char opt) {
+// val = calc(val, txt.getText().toString(), opt);
+     public double calc(double x, String input, char opt) {
         double y = Double.parseDouble(input);
         if (opt == '+') {
             return x + y;
