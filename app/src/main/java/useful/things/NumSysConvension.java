5ec6aa3bbb14c9972c4fee1a52ee98222e2dfaab
2003l <2002.leonid2003@gmail.com>
2019-12-30 18:34:45 +0300

    cooll

--- app/src/main/java/useful/things/NumSysConvension.java
+++ app/src/main/java/useful/things/NumSysConvension.java
@@ -1,70 +1,82 @@
-/*package useful.things;
+package useful.things;
 
-import android.widget.Switch;
-import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
 import java.math.BigInteger;
 
-public class NumSysConvension {
-    public String NumSysConvension (int number,String in, String out){
+// binary  decimal   hexadecimal  octal
+public class NumSysConvension extends AppCompatActivity {
+    public String NumSysConvension (String number,String in, String out){
         BigInteger dec;
-        switch (in) {
+
+        switch(in) {
             case "binary":
                 try {
-                    dec = new BigInteger(new BigInteger(String.valueOf(Integer.parseInt(vvodtxt.getText().toString()))).toString(10), 2);
-                    if (out =="decimal") return (Integer.toOctalString(Integer.parseInt(dec.toString())));
-                    if (out =="hexadecimal")txt1.setText(Integer.toHexString(Integer.parseInt(dec.toString())).toUpperCase() + " " + getResources().getString(R.string.in_the_hexadecimal_system));
-                    if (out =="octal") txt2.setText(String.valueOf(dec) + " " + getResources().getString(R.string.in_the_decimal_system));
-
+                    dec = new BigInteger(new BigInteger(String.valueOf(Integer.parseInt(number))).toString(10), 2);
+                    switch (out) {
+                        case "decimal": return (dec.toString());
+                        case "hexadecimal":return (Integer.toHexString(Integer.parseInt(dec.toString())).toUpperCase());
+                        case "octal":return (Integer.toOctalString(Integer.parseInt(in)));
+                    }
                 }
                 catch (Exception e) {
-                    Toast.makeText(NumSysCtivity.this, getResources().getText(R.string.errnumsys), Toast.LENGTH_SHORT).show();
+                    return (String) getResources().getText(R.string.errnumsys);
                 }
-
                 break;
+
             case "decimal":
                 try {
-                    txtotvet.setText(getResources().getString(R.string.in_the_decimal_system)+" "+getResources().getString(R.string.Number)+" " + vvodtxt.getText().toString()+" "+getResources().getString(R.string.Equal) +":");
-                    txt0.setText(Integer.toBinaryString(Integer.parseInt(vvodtxt.getText().toString())) + " "+getResources().getString(R.string.in_the_binary_system));
-                    txt1.setText(Integer.toOctalString(Integer.parseInt(vvodtxt.getText().toString()))+ " "+getResources().getString(R.string.in_the_octal_system));
-                    txt2.setText(Integer.toHexString(Integer.parseInt(vvodtxt.getText().toString())).toUpperCase()+ " "+getResources().getString(R.string.in_the_hexadecimal_system));
+                    dec = BigInteger.valueOf(Long.parseLong(number));
+                    switch (out) {
+                        case "binary": return Integer.toBinaryString(Integer.parseInt(dec.toString()));
+                        case "hexadecimal":return (Integer.toHexString(Integer.parseInt(dec.toString())).toUpperCase());
+                        case "octal":return (Integer.toOctalString(Integer.parseInt(dec.toString())));
+                    }
                 }
                 catch (Exception e) {
-                    Toast.makeText(NumSysCtivity.this, getResources().getText(R.string.errnumsys), Toast.LENGTH_SHORT).show();
+                    return (String) getResources().getText(R.string.errnumsys);
                 }
-
                 break;
+
             case "hexadecimal":
                 try {
-                    dec = BigInteger.valueOf(hextodec(vvodtxt.getText().toString().toUpperCase()));
-                    txtotvet.setText(getResources().getString(R.string.Hexadecimal)+" "+getResources().getString(R.string.Number)+" " + vvodtxt.getText().toString().toUpperCase()+" "+getResources().getString(R.string.Equal) +":");
-                    txt0.setText(Integer.toBinaryString(Integer.parseInt(dec.toString()))+ " "+getResources().getString(R.string.in_the_binary_system));
-                    txt1.setText(Integer.toOctalString(Integer.parseInt(dec.toString()))+ " "+getResources().getString(R.string.in_the_octal_system));
-                    txt2.setText(String.valueOf(dec)+ " "+getResources().getString(R.string.in_the_decimal_system));
+                    dec = BigInteger.valueOf(hextodec(number.toUpperCase()));
+                    switch (out) {
+                        case "binary": return Integer.toBinaryString(Integer.parseInt(dec.toString()));
+                        case "decimal": return (dec.toString());
+                        case "octal":return (Integer.toOctalString(Integer.parseInt(dec.toString())));
+                    }
                 }
                 catch (Exception e) {
-                    Toast.makeText(NumSysCtivity.this, getResources().getText(R.string.errnumsys), Toast.LENGTH_SHORT).show();
+                    return (String) getResources().getText(R.string.errnumsys);
                 }
                 break;
+
             case "octal":
                 try {
-                    dec =new BigInteger(new BigInteger(String.valueOf(Integer.parseInt(vvodtxt.getText().toString()))).toString(10),8);
-                    txtotvet.setText(getResources().getString(R.string.in_the_octal_system)+" "+getResources().getString(R.string.Number)+" " + vvodtxt.getText().toString()+" "+getResources().getString(R.string.Equal) +":");
-                    txt0.setText(Integer.toBinaryString(Integer.parseInt(dec.toString()))+ " "+getResources().getString(R.string.in_the_binary_system));
-                    txt1.setText(Integer.toHexString(Integer.parseInt(vvodtxt.getText().toString())).toUpperCase()+ " "+getResources().getString(R.string.in_the_hexadecimal_system));
-                    txt2.setText(String.valueOf(dec)+ " "+getResources().getString(R.string.in_the_decimal_system));
-                    break;
+                    dec = new BigInteger(new BigInteger(String.valueOf(Integer.parseInt(number))).toString(8), 2);
+                    switch (out) {
+                        case "binary": return Integer.toBinaryString(Integer.parseInt(number));
+                        case "decimal": return (dec.toString());
+                        case "hexadecimal":return (Integer.toHexString(Integer.parseInt(dec.toString())).toUpperCase());
+                    }
                 }
                 catch (Exception e) {
-                    Toast.makeText(NumSysCtivity.this, getResources().getText(R.string.errnumsys), Toast.LENGTH_SHORT).show();
+                    return (String) getResources().getText(R.string.errnumsys);
                 }
-            default:
                 break;
         }
-        return 0;
+        return (String) getResources().getText(R.string.errnumsys);
     }
+
+    public static int hextodec(String hex) {
+        String digits = "0123456789ABCDEF";
+        int val = 0;
+        for (int i = 0; i < hex.length(); i++) {
+            char j = hex.charAt(i);
+            int k = digits.indexOf(j);
+            val = 16*val + k;
+        }
+        return val;
+    }
+
 }
-
-// end
-
-        }*/
